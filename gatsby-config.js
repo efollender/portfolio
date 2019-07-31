195c8aa34c89955a56a6b059601a6e621d5774c5
Emma Follender <efollender@gmail.com>
2019-07-31 12:51:36 -0700

    Updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Emma Follender`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Emma Follender is a creative developer in Seattle`,
+    author: `@efollender`,
   },
   plugins: [
     `gatsby-plugin-stylus`,
