9f97eb616f522e07d7b444d972539c76cf2efb0b
Emma Follender <efollender@gmail.com>
2019-07-31 12:40:17 -0700

    Added rain

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, {Component} from "react"
 
 const navMenu = [
   {
@@ -13,29 +13,57 @@ const navMenu = [
   },
 ]
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className='content--wrapper'>
-      <h1>
-        Hi, I'm <Link to="/">
-          {siteTitle}
-        </Link>, a creative developer
-        based in  <Link to='/'>
-          {' Seattle'}
-        </Link>.
-      </h1>
-      <div className="nav--wrapper">
-        {navMenu.map(item =>
-          <div className='nav--item'>
-            <Link to={item.path} activeClassName='active'>
-              {item.label}
-            </Link>
-          </div>  
-        )}
-      </div>
-    </div>
-  </header>
-)
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      raining: false
+    }
+  }
+  rain = () => {
+    this.setState({
+      raining: true
+    })
+  }
+  stopRain = () => {
+    this.setState({
+      raining: false
+    })
+  }
+  render() {
+    const { siteTitle } = this.props;
+    return (
+      <header>
+        <div className='content--wrapper'>
+          <div className={`rain ${this.state.raining ? 'stormin' : 'nope'}`}>
+            {Array.from(new Array(10)).map(item => 
+              <div className="stem--wrapper">
+                <div className="stem"/>
+              </div>
+            )}
+          </div>
+          <h1>
+            Hi, I'm <Link to="/">
+              {siteTitle}
+            </Link>, a creative developer
+            based in  <a href='#' onMouseOver={this.rain} onMouseLeave={this.stopRain}>
+              {' Seattle'}
+            </a>.
+          </h1>
+          <div className="nav--wrapper">
+            {navMenu.map(item =>
+              <div className='nav--item'>
+                <Link to={item.path} activeClassName='active'>
+                  {item.label}
+                </Link>
+              </div>  
+            )}
+          </div>
+        </div>
+      </header>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const AboutPage = () => (
       It's been a wild ride.<br/><br/>
       After working for three years as a self-taught developer, her insatiable curiosity led her away from New York and landed her at a coding bootcamp in Austin, Texas.
       <br/><br/>
-      There she built a full-stack foundation, but found that her passion lied solidly in building engaging UX. She is happiest when working closely with designers to push the boundaries of what's possible.
+      There she built a full-stack foundation, but found that her passion lied solidly in building engaging UX. She is happiest when working closely with designers to create the unexpected.
       <br/><br/>
       She thrives on the pace of agencies and has worked at both <a href='https://brooklynfoundry.com/'>Brooklyn Digital Foundry</a> in New York and <a href="https://www.instrument.com">Instrument</a> in Portland, OR.
       <br/><br/>
